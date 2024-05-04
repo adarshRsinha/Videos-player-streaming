d453cdb3568ad33749101886cbfdfcc2cadc9c65
adarshRsinha <93790691+adarshRsinha@users.noreply.github.com>
2024-05-04 15:29:57 +0530

    first commit

---
+++ frontend/video-streaming/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer';
+import { useRef } from 'react';
+import background from './images/img1-design.jpg';
+
+function App() {
+  const playerRef = useRef(null);
+  const videoLink =
+  "http://localhost:8000/uploads/courses/91627120-2fb3-4ecb-bfee-9d0f93c252f6/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
+  return (
+    <>
+      {/* <div>
+        <h1>Video player</h1>
+      </div> */}
+
+      <div className="flex flex-col pb-20 bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
+        <header className="flex gap-5 justify-between px-8 py-6 w-full text-white max-md:flex-wrap max-md:px-5 max-md:max-w-full">
+          <div className="flex gap-3 text-3xl font-bold whitespace-nowrap">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/8158d20177eeaf79c24e75960c2132f560f353fb66ddeab755557e1494f67174?apiKey=3f2c1e08085a404c8c7a95f15a9feb2b&"
+              alt="WePlay logo"
+              className="shrink-0 aspect-square w-[57px]"
+            />
+            <div className="flex-auto my-auto">WePlay</div>
+          </div>
+         
+        </header>
+        <main className="flex z-10 gap-5 self-center mt-14 max-w-full text-white w-[771px] max-md:flex-wrap max-md:mt-10">
+          <section className="flex flex-col grow shrink-0 px-5 basis-0 w-fit max-md:max-w-full">
+            <h1 className="text-6xl font-bold text-center max-md:max-w-full max-md:text-4xl">
+              Online Video Player & <br /> Platform Solution
+            </h1>
+            <p className="mt-16 text-3xl max-md:mt-10 max-md:max-w-full">
+              Explore on the world's best Online Video Player
+            </p>
+            
+          </section>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/146a598697023fe7cdad6f7b6fc918d32143a73e13ea565cec11c3192f066380?apiKey=3f2c1e08085a404c8c7a95f15a9feb2b&"
+            alt=""
+            className="shrink-0 self-start mt-2 w-6 aspect-[0.65] blur-[2px] fill-cyan-400"
+          />
+        </main>
+        <div className="flex flex-col justify-center items-center self-center mt-56 w-full max-w-[1101px] max-md:mt-10 max-md:max-w-full">
+          <VideoPlayer
+            options={videoPlayerOptions}
+            onReady={handlePlayerReady}
+          />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,92 @@
+// const express = require('express');
+import express from 'express'; //because in package.json we define thr type module
+// const multer = require('multer');
+import multer from 'multer';
+// const cors = require('cors');
+import cors from 'cors';
+// const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import fs from 'fs';
+import {exec} from 'child_process';
+import { stderr, stdout } from 'process';
+
+const app = express();
+
+//multer middleware
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "./uploads");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+    }
+})
+
+//multer configuration
+const upload = multer({storage: storage});
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://127.0.0.1:5173"],
+        credentials: true,
+    })
+)
+
+//middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  //Imp
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+    next();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+    return res.json({message: "Hello Its working"})
+})
+
+app.post("/upload", upload.single('file'), function(req, res){
+    // console.log("file uploaded");
+    const lessionId = uuidv4();
+    const videoPath = req.file.path;
+    const outputPath = `./uploads/courses/${lessionId}`;
+    const hlsPath = `${outputPath}/index.m3u8`;
+    console.log('hlsPath', hlsPath);
+
+    if(!fs.existsSync(outputPath)){
+        fs.mkdirSync(outputPath, {recursive: true})
+    }
+
+    //ffmpeg {magic part}
+    // command to convert video to HLS format using ffmpeg
+
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+
+  // run the ffmpeg command; usually done in a separate process (queued)
+  //No queue because of POC, not to be used in production
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if(error){
+        console.log(`exec error:, ${error}`);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    const videoUrl = `http://localhost:8000/uploads/courses/${lessionId}/index.m3u8`;
+
+    res.json({
+        message: "Videos converted to HLS format",
+        videoUrl: videoUrl,
+        lessionId: lessionId,
+    })
+  })
+
+})
+
+app.listen(8000, (req, res) => {
+    console.log("server is started");
+})
